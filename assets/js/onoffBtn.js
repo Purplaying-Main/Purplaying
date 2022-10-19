05f88839aec0232b873e949d43fbf86fc36cc4d5
he22ra <115213118+he22ra@users.noreply.github.com>
2022-10-19 17:55:12 +0900

    10/19 1차 파일 병합
    CSS, html 파일만 병합

---
+++ assets/js/onoffBtn.js
@@ -0,0 +1,43 @@
+angular.module('uiSwitch', [])
+
+.directive('switch', function(){
+  return {
+    restrict: 'AE'
+  , replace: true
+  , transclude: true
+  , template: function(element, attrs) {
+      var html = '';
+      html += '<span';
+      html +=   ' class="switch' + (attrs.class ? ' ' + attrs.class : '') + '"';
+      html +=   attrs.ngModel ? ' ng-click="' + attrs.disabled + ' ? ' + attrs.ngModel + ' : ' + attrs.ngModel + '=!' + attrs.ngModel + (attrs.ngChange ? '; ' + attrs.ngChange + '()"' : '"') : '';
+      html +=   ' ng-class="{ checked:' + attrs.ngModel + ', disabled:' + attrs.disabled + ' }"';
+      html +=   '>';
+      html +=   '<small></small>';
+      html +=   '<input type="checkbox"';
+      html +=     attrs.id ? ' id="' + attrs.id + '"' : '';
+      html +=     attrs.name ? ' name="' + attrs.name + '"' : '';
+      html +=     attrs.ngModel ? ' ng-model="' + attrs.ngModel + '"' : '';
+      html +=     ' style="display:none" />';
+      html +=     '<span class="switch-text">'; /*adding new container for switch text*/
+      html +=     attrs.on ? '<span class="on">'+attrs.on+'</span>' : ''; /*switch text on value set by user in directive html markup*/
+      html +=     attrs.off ? '<span class="off">'+attrs.off + '</span>' : ' ';  /*switch text off value set by user in directive html markup*/
+      html += '</span>';
+      return html;
+    }
+  }
+});
+
+
+angular.module('app', ['uiSwitch'])
+
+.controller('MyController', function($scope) {
+  $scope.enabled = true;
+  $scope.onOff = true;
+  $scope.yesNo = true;
+  $scope.disabled = true;
+
+
+  $scope.changeCallback = function() {
+    console.log('This is the state of my model ' + $scope.enabled);
+  };
+});
