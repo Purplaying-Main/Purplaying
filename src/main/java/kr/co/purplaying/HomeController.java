28575519f1876c37358c961613022b9b356fb887
BlueUmb <88631319+BlueUmb@users.noreply.github.com>
2022-10-31 15:14:59 +0900

    Update HomeController.java

--- src/main/java/kr/co/purplaying/HomeController.java
+++ src/main/java/kr/co/purplaying/HomeController.java
@@ -16,30 +16,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/temp", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-	
-		//index.html호출
-		@RequestMapping(value="/", method=RequestMethod.GET)
-		public String index() {
-			return "index";
-		}
+    
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+    
+    /**
+     * Simply selects the home view to render by returning its name.
+     */
+    @RequestMapping(value = "/temp", method = RequestMethod.GET)
+    public String home(Locale locale, Model model) {
+        logger.info("Welcome home! The client locale is {}.", locale);
+        
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+        
+        String formattedDate = dateFormat.format(date);
+        
+        model.addAttribute("serverTime", formattedDate );
+        
+        return "home";
+    }
+    
+        //index.html호출
+        @RequestMapping(value="/", method=RequestMethod.GET)
+        public String index() {
+            return "index";
+        }
 }
 
