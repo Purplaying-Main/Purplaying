6ee29f1940bf3e21557461d8fd5a3006cad90e05
yeongeun-Kim <1787forwork@gmail.com>
2022-11-02 00:21:34 +0900

    projectdetail 페이지 찜하기 JS (스위치기능추가필요)

---
+++ src/main/webapp/resources/assets/js/pickBtn.js
@@ -0,0 +1,11 @@
+/**
+ * 찜하기 버튼 클릭시 색상 변화 JS (스위치 기능 필요)
+ */
+ 	const pick = document.getElementById("pickBtn");
+ 	function pickFunding(){
+    pick.style.color = "red";
+    pick.style.fontWeight = "bold";
+ 	}
+
+
+
