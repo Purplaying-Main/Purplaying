75cbd38a673ec58d9997beb1aeccd1d537a29efc
jake852 <jake852@daum.net>
2022-11-03 13:57:42 +0900

    error 추가

--- src/main/java/kr/co/purplaying/pageController.java
+++ src/main/java/kr/co/purplaying/pageController.java
@@ -1,6 +1,9 @@
 package kr.co.purplaying;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -212,6 +215,23 @@ public class pageController {
     public String privacypolicy() {
         return "privacypolicy";
     }
+
+    // error
+    // 첫번째 오류: 0으로 나눴으므로 500 오류를 발생시킨다.
+    @GetMapping("/divide")
+    public String problem(Model m) {
+        int result = 1 / 0;
+        m.addAttribute("serverTime", result);
+        return "error";
+    }
+
+    // 두 번째 오류: @PostMapping이므로 Get 방식으로 요청 시 405 오류를 발생시킨다.
+    @PostMapping("/method")
+    public String method(Model m) {
+        return "error";
+    }
+
+
 }
     
     
