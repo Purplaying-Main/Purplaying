a480c123b6fd82d03165cf00d6b8630f39dff571
sh9709 <kidwars82@gmail.com>
2022-11-17 17:03:19 +0900

    11-17

--- src/main/java/kr/co/purplaying/dao/UserDao.java
+++ src/main/java/kr/co/purplaying/dao/UserDao.java
@@ -7,7 +7,7 @@ public interface UserDao {
 	UserDto selectUser(String user_id) throws Exception;
 	int deleteAll() throws Exception ;
 	int count() throws Exception;
-	int insertUser(UserDto dto) throws Exception;
+	int insertUser(UserDto userDto) throws Exception;
     int signUpUser(String user_id, String user_pwd, String user_name, String user_phone) throws Exception;
     int userCheck(int user_no,boolean agree1,boolean agree2,boolean agree3,boolean agree4, boolean agree5) throws Exception;
     UserDto searchUser_no(String user_id) throws Exception;
