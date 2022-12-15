a52819cdd40cd197b29eab7f17714541730107eb
he22ra <hhraleeee@gmail.com>
2022-12-15 19:17:49 +0900

    마이페이지 알림 dto, dao, service 생성

---
+++ src/main/java/kr/co/purplaying/dao/AlarmDao.java
@@ -0,0 +1,18 @@
+package kr.co.purplaying.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import kr.co.purplaying.domain.AlarmDto;
+
+public interface AlarmDao {
+
+  List<AlarmDto> selectPage(Map map) throws Exception;
+
+  int count() throws Exception;
+
+  AlarmDto selectAlarm(int alarm_no) throws Exception;
+
+  int insertAlarm(AlarmDto alarmDto) throws Exception;
+
+}
---
+++ src/main/java/kr/co/purplaying/service/AlarmService.java
@@ -0,0 +1,20 @@
+package kr.co.purplaying.service;
+
+import java.util.List;
+import java.util.Map;
+
+import kr.co.purplaying.domain.AlarmDto;
+
+public interface AlarmService {
+
+  List<AlarmDto> getPage(Map map) throws Exception;
+
+  int getCount() throws Exception;
+
+  AlarmDto selectAlarm(int alarm_no) throws Exception;
+
+  int insertAlarm(AlarmDto alarmDto) throws Exception;
+
+  List<AlarmDto> selectPage(Map map) throws Exception;
+
+}
